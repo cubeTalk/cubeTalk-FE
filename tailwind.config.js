02fec16d1d5877032042a0fc6441c698b9253682
eunsik-kim <gracely9901@gmail.com>
2024-08-28 18:31:59 +0900

    chore:style component설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      ss: "480px",
+      sm: "620px",
+      sl: "768px",
+      md: "1060px",
+      lg: "1200px",
+    },
   },
   plugins: [],
-}
-
+};
