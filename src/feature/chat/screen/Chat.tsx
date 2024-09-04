5d703338b14f5d0b9c446c8a55ea48e0ac324cb3
eunsik-kim <gracely9901@gmail.com>
2024-09-05 01:23:14 +0900

    feat: 사회자 bubble UI 구현

--- src/feature/chat/screen/Chat.tsx
+++ src/feature/chat/screen/Chat.tsx
@@ -3,15 +3,25 @@ import MessageInput from "../component/MessageInput";
 import { colflex, scrollBar } from "../../../styles/shared";
 import { Bubble } from "../component/Bubbles";
 import { ChangeTeamBubble, EnterBubble } from "../component/RoomBubbles";
+import ModeratorBubble from "../component/ModeratorBubble";
 
 const Chat = () => {
   return (
     <ScreenLayout>
       <h2>전체 채팅</h2>
       <BubbleContainer>
-        {Array.from({length: 20}, () => <Bubble />)}
+        {Array.from({ length: 5 }, () => (
+          <Bubble />
+        ))}
         <EnterBubble userName="배배고고파파" isEnter={true} />
         <ChangeTeamBubble userName="배배고고파파" team="찬성팀" />
+        <ModeratorBubble
+          order={0}
+          topic="이대로 취업을 할 수 있을것인가...!이대로 취업을 할 수 있을것인가...!이대로 취업을 할 수 있을것인가...!"
+        />
+        <ModeratorBubble order={1} topic="이대로 취업을 할 수 있을것인가...!" />
+        <ModeratorBubble order={6} />
+        <ModeratorBubble order={7} />
         <Bubble isleft={false} />
       </BubbleContainer>
       <MessageInput />
@@ -35,4 +45,4 @@ const BubbleContainer = styled.div`
   gap: 10px;
   padding: 5px 10px 20px 0px;
   overflow-y: auto;
-`
+`;
