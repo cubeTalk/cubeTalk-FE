17742caf3c4233999efcb2e7bf20127df5573850
eunsik-kim <gracely9901@gmail.com>
2024-09-17 07:13:37 +0900

    feat: web socket instance 생성

---
+++ src/shared/webSocket/index.ts
@@ -0,0 +1,130 @@
+import * as StompJs from "@stomp/stompjs";
+import { ChatMessage, ReadyMessage, VoteMessage } from "../type";
+
+interface ConnectArgs {
+  id: string;
+  mainChatId: string;
+  subChatId: string;
+  nickName: string;
+  mainChatCallback: (message: StompJs.IMessage) => void;
+  subChatCallback: (message: StompJs.IMessage) => void;
+  progressCallback: (message: StompJs.IMessage) => void;
+  participantsCallback: (message: StompJs.IMessage) => void;
+  errorCallback: (message: StompJs.IMessage) => void;
+}
+
+class WebSocketManager {
+  private client: StompJs.Client | null = null;
+  private connected: boolean = false;
+  private chatRoomId: string = "";
+
+  connect = ({
+    id,
+    mainChatId,
+    subChatId,
+    nickName,
+    mainChatCallback,
+    subChatCallback,
+    progressCallback,
+    participantsCallback,
+    errorCallback,
+  }: ConnectArgs) => {
+    if (this.client) {
+      this.client.deactivate();
+    }
+
+    this.client = new StompJs.Client({
+      brokerURL: `${import.meta.env.VITE_SOCKET}ws`,
+      connectHeaders: {
+        id,
+        nickName,
+      },
+      debug: (str) => {
+        console.log(str);
+      },
+      reconnectDelay: 4000,
+      heartbeatIncoming: 10000,
+      heartbeatOutgoing: 10000,
+      onConnect: () => {
+        if (this.client) {
+          this.client.subscribe(`/topic/chat.${mainChatId}`, mainChatCallback);
+          this.client.subscribe(`/topic/chat.${subChatId}`, subChatCallback);
+          this.client.subscribe(`/topic/progress.${id}`, progressCallback);
+          this.client.subscribe(`/topic/${id}.participants.list`, participantsCallback);
+          this.client.subscribe(`/topic/error`, errorCallback);
+
+          this.connected = true;
+          this.chatRoomId = id;
+          console.log("Connected");
+        } else {
+          console.log("Connecting failed");
+        }
+      },
+      onStompError: (frame) => {
+        console.error("Broker reported error: " + frame.headers["message"]);
+        console.error("Additional details: " + frame.body);
+      },
+      onDisconnect: () => {
+        console.log("Disconnected");
+        this.connected = false;
+      },
+    });
+    this.client.activate();
+  };
+
+  disconnect = () => {
+    if (this.client) {
+      this.client.deactivate();
+      this.client = null;
+    }
+  };
+
+  changeTeam = (
+    oldSubChatId: string,
+    newSubCHatId: string,
+    chatCallback: (message: StompJs.IMessage) => void
+  ) => {
+    if (this.client && this.connected) {
+      this.client.unsubscribe(`/topic/chat.${oldSubChatId}`);
+      this.client.subscribe(`/topic/chat.${newSubCHatId}`, chatCallback);
+    }
+  };
+
+  sendMessage = (channelId: string, chatMessage: ChatMessage) => {
+    if (this.client && this.connected) {
+      this.client.publish({
+        destination: `/pub/message/${channelId}`,
+        body: JSON.stringify(chatMessage),
+      });
+    } else {
+      console.log("Sending Message failed");
+    }
+  };
+
+  voteMessage = (voteMessage: VoteMessage) => {
+    if (this.client && this.connected) {
+      this.client.publish({
+        destination: `/pub/${this.chatRoomId}/vote`,
+        body: JSON.stringify(voteMessage),
+      });
+    } else {
+      console.log("voting Message failed");
+    }
+  };
+
+  ReadyMessage = (voteMessage: ReadyMessage) => {
+    if (this.client && this.connected) {
+      this.client.publish({
+        destination: `/pub/${this.chatRoomId}/ready`,
+        body: JSON.stringify(voteMessage),
+      });
+    } else {
+      console.log("voting Message failed");
+    }
+  };
+  isConnected() {
+    return this.connected;
+  }
+}
+
+export default new WebSocketManager();
