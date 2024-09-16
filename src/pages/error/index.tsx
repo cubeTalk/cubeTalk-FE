43ad7d09315aed445eca6fd861ea72314c92544e
eunsik-kim <gracely9901@gmail.com>
2024-09-16 23:22:56 +0900

    feat: error page 세분화

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,18 +1,45 @@
-import { useState } from "react";
-import Modal from "../../shared/components/modal";
+import { Link } from "react-router-dom";
+import Header from "../../app/router/ui/header";
+import Footer from "../../app/router/ui/footer";
+import styled from "styled-components";
+import { rowflex } from "../../shared/style/commonStyle";
 
-const ErrorPage = () => {
-  const [modalVisible, setModalVisible] = useState(false);
+export const HomeErrorPage = () => {
   return (
-    <>
-      <button onClick={() => setModalVisible(true)}>모달열기</button>
-      {modalVisible && (
-        <Modal closeModal={() => setModalVisible(false)}>
-          <p>모달입니다.</p>
-        </Modal>
-      )}
-    </>
+    <div className="w-full">
+      <Header />
+      <div className="flex flex-col justify-center items-center h-[40vh] text-xl">
+        <h3>에러가 발생했습니다. 새로고침을 눌러 다시 접속해 주세요</h3>
+      </div>
+      <Footer />
+    </div>
   );
 };
 
-export default ErrorPage;
+export const DebateErrorPage = () => {
+  return (
+    <div className="w-full">
+      <div className="flex flex-col justify-center items-center h-[100vh] text-xl">
+        <h3>에러가 발생했습니다 메인화면으로 돌아가 주세요</h3>
+        <Link to={"/"}>
+          <LogoContainer>
+            <img alt="logo" src="/Icon/logo.png" />
+            <h1>CubeTalk</h1>
+          </LogoContainer>
+        </Link>{" "}
+      </div>
+    </div>
+  );
+};
+
+const LogoContainer = styled.div`
+  ${rowflex}
+  img {
+    width: 40px;
+    height: 40px;
+    margin-right: 8px;
+  }
+  h1 {
+    color: var(--white);
+  }
+`;
