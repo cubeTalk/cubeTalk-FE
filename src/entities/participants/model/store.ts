0da86cb2ea69683a89e01490858bfcf429f34395
eunsik-kim <gracely9901@gmail.com>
2024-09-19 06:43:56 +0900

    feat: 참가자 목록  status에 따른 방장 상태 구독

--- src/entities/participants/model/store.ts
+++ src/entities/participants/model/store.ts
@@ -3,6 +3,8 @@ import { combine, subscribeWithSelector } from "zustand/middleware";
 import { Participant } from "../../../shared/type";
 import { createModalStore } from "../../../shared/components/modal/model/store";
 import { useMainMessageStore } from "../../../widgets/mainChat/model/store";
+import { useisOwnerStore } from "../../../features/createDebate/model/store";
+import { useUserInfoStore } from "../../debateInfo";
 
 const initalParticipantsState = {
   participants: [] as Participant[],
@@ -28,5 +30,13 @@ useParticipantsStore.subscribe(
   (state) => state.participants,
   (participants) => {
     useMainMessageStore.getState().actions.resetParticipants(participants);
+    const memberId = useUserInfoStore.getState().memberId;
+    const me = participants.find((participants) => participants.memberId === memberId);
+    const actions = useisOwnerStore.getState().actions;
+    if (me?.status === "OWNER") {
+      actions.setIsOwner();
+    } else {
+      actions.setIsNotOwner();
+    }
   }
 );
